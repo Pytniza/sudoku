28f61b5969b18cce6ce9099ab19c6979f89ae98c
Pytniza <ip.web.so@gmail.com>
2019-02-19 02:39:17 +0300

    feat: decide task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function propose(row,col) {
+    let res = [];
+    let area = {
+        row: Math.floor(row/3)*3,
+        col: Math.floor(col/3)*3,
+    }
+    for (let i = 0; i < 9; i++) {
+        res.push([matrix[row][i], matrix[i][col], matrix[area.row + i % 3][area.col + Math.floor(i / 3)]])
+    }
+    return res;
+  }
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrix[row][col] === 0) {
+        let proposals = propose(row, col);
+        for (let proposal of proposals) {
+          matrix[row][col] = proposal;
+          solveSudoku(matrix);
+        }  
+      }
+    }
+  }
+  return matrix;
 }
